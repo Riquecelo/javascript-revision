5aae4e323e678dfb79dcfd587f4a1fa8e136a0ae
Marcelo Santos <marcelo.has@hotmail.com>
2026-08-16 00:59:26 -0300

    primeiros exemplos de função

---
+++ Functions/function-anonymous.js
@@ -0,0 +1,8 @@
+//Função anônima ou expressão de função
+
+// Função recebendo Parâmetros.
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // Invocação da função recebendo Argumentos
---
+++ Functions/function.js
@@ -0,0 +1,17 @@
+console.log("Hello word!")
+console.log("---------------\n")
+
+// Função nomeada
+// declaração da função
+function creatPharses() {
+    console.log("Estudar é bom!")
+    console.log("Persistir e Paciência!")
+    console.log("Repetição é a mãe da retenção!")
+}
+
+// Invocando a função
+creatPharses()
+
+console.log("\n---------------")
+console.log("Fim do programa")
+console.log("---------------")
