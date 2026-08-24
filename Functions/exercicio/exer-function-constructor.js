55b928701c5bbdc230ab5cd8b17cbb04134bd6f9
Marcelo Santos <marcelo.has@hotmail.com>
2026-08-23 23:02:07 -0300

    exercicio função construtora

--- Functions/exercicio/exer-function-constructor.js
+++ Functions/exercicio/exer-function-constructor.js
@@ -1,8 +1,6 @@
 /*
 🎯 Desafio 6 — Função construtora
-
 Agora imagine que o Back-End começou a enviar novos produtos.
-
 Crie uma função construtora chamada Produto.
 
 Ela deverá receber:
@@ -22,3 +20,30 @@ Preço: R$300
 Estoque: 7
 Categoria: Roupas
 */
+
+function Produto (nome,preco,estoque,categoria){
+    this.nome = nome
+    this.preco = preco
+    this.estoque = estoque
+    this.categoria = categoria
+    this.mostrarInformacoes = function(){
+        return ( 
+            `
+            Produto: ${this.nome} 
+            Preço: R$${this.preco }
+            Estoque: ${this.estoque }
+            Categoria: ${this.categoria}
+            `
+        )
+    }
+}
+
+let roupa1 = new Produto(
+    "Moletom Adidas",
+    300,
+    7,
+    "Roupas"
+)
+
+console.log(roupa1)
+console.log(roupa1.mostrarInformacoes())
