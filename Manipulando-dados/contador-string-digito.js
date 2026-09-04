20f5f9878a5754ef18f16ae2abf6d9ba6b0c95b0
Marcelo Santos <riquecelox462m@gmail.com>
2026-09-04 01:08:57 -0300

    manipulação de dados com javascript

---
+++ Manipulando-dados/contador-string-digito.js
@@ -0,0 +1,7 @@
+//Contar quantos caracteres tem uma palavra e quantos dígitos tem um número.
+
+let word = "paralelepipedo"
+console.log(word.length)
+
+let number = 2026
+console.log(String(number).length)
