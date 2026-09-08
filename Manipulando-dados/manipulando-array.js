441f248f1bb9fd6c0cb40fe4a7aaaa3f3cbb5972
Marcelo Santos <riquecelox462m@gmail.com>
2026-09-08 19:30:38 -0300

    manipulando array

--- Manipulando-dados/manipulando-array.js
+++ Manipulando-dados/manipulando-array.js
@@ -20,3 +20,30 @@ let array3 = Array.from(word) //pode-se usar o objeto Array com o método from()
 
 console.log(word.length)
 console.log(array3)
+
+let techs = ["html","css","js"]
+
+//adicionar um item no fim
+techs.push('node.js')
+
+//adicionar no começo
+techs.unshift('sql')
+
+//remover do fim
+techs.pop()
+
+//remover do começo
+techs.shift()
+
+//pegar somente alguns elementos do array
+console.log(techs.slice(1,3)) //slice não altera o array
+
+//remover um ou mais itens em qualquer posição do array
+//techs.splice(0, 2)
+
+//encontar a posição de um elemento no array
+let index = techs.indexOf('js')
+
+
+console.log('\n//////////////////////////////////////////////\n')
+console.log(index)
