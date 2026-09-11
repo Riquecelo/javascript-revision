31f323fe3270cd6388516bbdc3d97b3b65e8a28e
Marcelo Santos <riquecelox462m@gmail.com>
2026-09-11 17:21:34 -0300

    exercicios manipulando array

---
+++ Manipulando-dados/exercicios/exer-elementos-array.js
@@ -0,0 +1,31 @@
+/* Elementos do Array
+Dado:
+const produtos = [
+    "Notebook",
+    "Mouse",
+    "Teclado",
+    "Monitor"
+]
+
+Mostre:
+
+Primeiro produto:
+Último produto:
+Quantidade de produtos:
+
+Sem escrever manualmente o último índice.
+
+Ou seja, utilize:
+
+produtos.length */
+
+const produtos = [
+    "Notebook",
+    "Mouse",
+    "Teclado",
+    "Monitor"
+]
+
+console.log(produtos.length)
+console.log('Primeiro produto', produtos[0])
+console.log('Último produto', produtos[3])
