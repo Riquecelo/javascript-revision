12688dbcf2b9d97b31a318c81dd8a7cdb5e0c3f5
Marcelo Santos <riquecelox462m@gmail.com>
2026-09-19 14:53:35 -0300

    remoção de comentário

--- Manipulando-dados/exercicios/metodos-array/exer-reduce.js
+++ Manipulando-dados/exercicios/metodos-array/exer-reduce.js
@@ -1,7 +1,7 @@
 //Primeiro exercício
 //Complete o código para descobrir o total:
 
-/* const numeros = [5, 10, 15, 20];
+const numeros = [5, 10, 15, 20];
 
 const total1 = numeros.reduce((acumulador, numero) => {
     return acumulador + numero
@@ -15,11 +15,11 @@ const produtos1 = [
     { nome: "Teclado", preco: 300 }
 ];
 
-const total = produtos1.reduce((acumulador, produto) => {
+const total2 = produtos1.reduce((acumulador, produto) => {
     return acumulador + produto.preco
 },0)
 
-console.log(total)
+console.log(total2)
 
 const produtos = [
     { nome: "Notebook", quantidade: 2 },
@@ -29,10 +29,10 @@ const produtos = [
 
 const totalProdutos = produtos.reduce((acumulador,produto) => acumulador + produto.quantidade,0)
 console.log(totalProdutos)
- */
 
-const numeros = [10, 20, 30];
 
-const total = numeros.reduce((acumulador, numero) =>  acumulador + numero, 0);
+const numeros2 = [10, 20, 30];
+
+const total = numeros2.reduce((acumulador, numero) =>  acumulador + numero, 0);
 
 console.log(total);
